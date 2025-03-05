4f72f32095e1afa54ea5ea23b31b047bfa142934
an-adventure-awaits <amcintire12345@gmail.com>
2025-03-04 21:04:11 -0700

    Updated background and 2 buttons

--- calculator.py
+++ calculator.py
@@ -6,27 +6,25 @@ Purpose:
 """
 
 # import tkinter
-import tkinter
-import ttkbootstrap as ttk
-
+import tkinter as tk
 
 class Calculator:
 
     def root(self):
-        self.root = ttk
+        self.root = tk.Tk()
 
         self.root.title("CALCULATOR")
 
-        self.root.geometry("400x300")
+        self.root.geometry("300x400")
 
         self.root.configure(bg='pink')
 
 
         # button
-        self.button_1 = ttk.Button(
+        self.button_1 = tk.Button(
             self.root,
             text="Calculator",
-
+            
             # text position
             #command=click_button,
 
@@ -38,12 +36,12 @@ class Calculator:
 
     # ---------- CREATE WIDGETS --------- #
     def create_widgets(self):
-        self.btn_calculator = ttk.Button(
+        self.btn_calculator = tk.Button(
             text="Calculate",
-            command=self.get_data
+            #command=self.get_data
         )
 
-        self.lbl_calculator = ttk.Label(
+        self.lbl_calculator = tk.Label(
             wraplength=250,
             justify="left"
         )
@@ -53,17 +51,19 @@ class Calculator:
         self.lbl_calculator.grid(row=1, column=0, padx=PAD, pady=PAD, sticky='W')
 
         # The enter key will activate the calculate method
-        self.root.bind("<Return>", self.get_data)
-        self.root.bind("<KP_Enter>", self.get_data)
-        self.root.bind("<Return>", self.close_window)
+        #self.root.bind("<Return>", self.get_data)
+        #self.root.bind("<KP_Enter>", self.get_data)
+       
 
-        
+           
 
 calculator = Calculator()
 
-def main():
-    calculator.root()
-    calculator.create_widgets()
+
+calculator.root()
+calculator.create_widgets()
+calculator.root.mainloop()
+    
 
 
 
