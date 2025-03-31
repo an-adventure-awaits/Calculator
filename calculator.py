8c3b4568d2fb716370b0181fcb4d9e498bd11ce9
an-adventure-awaits <amcintire12345@gmail.com>
2025-03-31 16:09:31 -0600

    More button positioning

--- calculator.py
+++ calculator.py
@@ -15,7 +15,7 @@ class Calculator:
 
         self.root.title("CALCULATOR")
 
-        self.root.geometry("300x400")
+        self.root.geometry("400x500")
 
         self.root.configure(bg='pink')
 
@@ -32,13 +32,13 @@ class Calculator:
             relief="groove"
             )
 
-        self.button_1.grid(row=1, column=2)
+        self.button_1.grid(row=1, column=3)
         
 
     # ---------- CREATE WIDGETS --------- #
     def create_widgets(self):
 
-        # button 1
+# button 1
         self.btn_calculator = tk.Button(
                 text="1",
                 #command=self.get_data
@@ -49,11 +49,11 @@ class Calculator:
             justify="left"
         )
 
-        PAD = 5
+        PAD = 3
         self.btn_calculator.grid(row=5, column=1, padx=PAD, pady=PAD, sticky='W')
         self.lbl_calculator.grid(row=5, column=1, padx=PAD, pady=PAD, sticky='W')
 
-        # button 2
+# button 2
         self.btn_calculator = tk.Button(
             text="2",
             #command=self.get_data
@@ -64,11 +64,11 @@ class Calculator:
             justify="left"
         )
 
-        PAD = 5
+        PAD = 3
         self.btn_calculator.grid(row=5, column=2, padx=PAD, pady=PAD, sticky='W')
         self.lbl_calculator.grid(row=5, column=2, padx=PAD, pady=PAD, sticky='W')
         
-        # button 3
+# button 3
         self.btn_calculator = tk.Button(
             text="3",
             #command=self.get_data
@@ -79,11 +79,11 @@ class Calculator:
             justify="left"
         )
 
-        PAD = 10
-        self.btn_calculator.grid(row=5, column=2, padx=PAD, pady=PAD, sticky='E')
-        self.lbl_calculator.grid(row=5, column=2, padx=PAD, pady=PAD, sticky='E')
+        PAD = 3
+        self.btn_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='E')
+        self.lbl_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='E')
 
-        # button 4
+# button 4
         self.btn_calculator = tk.Button(
                 text="4",
                 #command=self.get_data
@@ -94,11 +94,11 @@ class Calculator:
             justify="left"
         )
 
-        PAD = 10
-        self.btn_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
-        self.lbl_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
+        PAD = 3
+        self.btn_calculator.grid(row=5, column=4, padx=PAD, pady=PAD, sticky='W')
+        self.lbl_calculator.grid(row=5, column=4, padx=PAD, pady=PAD, sticky='W')
 
-        # button 5
+# button 5
         self.btn_calculator = tk.Button(
                 text="5",
                 #command=self.get_data
@@ -109,11 +109,11 @@ class Calculator:
             justify="left"
         )
 
-        PAD = 10
-        self.btn_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
-        self.lbl_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
+        PAD = 3
+        self.btn_calculator.grid(row=7, column=1, padx=PAD, pady=PAD, sticky='W')
+        self.lbl_calculator.grid(row=7, column=1, padx=PAD, pady=PAD, sticky='W')
         
-        # button 6
+# button 6
         self.btn_calculator = tk.Button(
                 text="6",
                 #command=self.get_data
@@ -124,11 +124,11 @@ class Calculator:
             justify="left"
         )
 
-        PAD = 10
-        self.btn_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
-        self.lbl_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
+        PAD = 3
+        self.btn_calculator.grid(row=7, column=2, padx=PAD, pady=PAD, sticky='W')
+        self.lbl_calculator.grid(row=7, column=2, padx=PAD, pady=PAD, sticky='W')
 
-        # button 7
+# button 7
         self.btn_calculator = tk.Button(
                 text="7",
                 #command=self.get_data
@@ -139,9 +139,56 @@ class Calculator:
             justify="left"
         )
 
-        PAD = 10
+        PAD = 3
+        self.btn_calculator.grid(row=7, column=3, padx=PAD, pady=PAD, sticky='W')
+        self.lbl_calculator.grid(row=7, column=3, padx=PAD, pady=PAD, sticky='W') 
+
+# button 8
+        self.btn_calculator = tk.Button(
+                text="8",
+                #command=self.get_data
+            )
+
+        self.lbl_calculator = tk.Label(
+            wraplength=250,
+            justify="left"
+        )
+
+        PAD = 3
         self.btn_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
-        self.lbl_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')   
+        self.lbl_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')  
+
+# button 9
+        self.btn_calculator = tk.Button(
+                text="9",
+                #command=self.get_data
+            )
+
+        self.lbl_calculator = tk.Label(
+            wraplength=250,
+            justify="left"
+        )
+
+        PAD = 3
+        self.btn_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
+        self.lbl_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
+
+# button 0 
+        self.btn_calculator = tk.Button(
+                text="0",
+                #command=self.get_data
+            )
+
+        self.lbl_calculator = tk.Label(
+            wraplength=250,
+            justify="left"
+        )
+
+        PAD = 3
+        self.btn_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W')
+        self.lbl_calculator.grid(row=5, column=3, padx=PAD, pady=PAD, sticky='W') 
+
+
         # The enter key will activate the calculate method
         #self.root.bind("<Return>", self.get_data)
         #self.root.bind("<KP_Enter>", self.get_data)
